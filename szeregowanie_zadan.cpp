72b14eac1fb7e2a2ce024abd2fb05e682c10f880
mayilook <damian@descreen.pl>
2017-03-09 10:37:59 +0100

    heapsort

--- szeregowanie_zadan.cpp
+++ szeregowanie_zadan.cpp
@@ -11,13 +11,6 @@ struct Zadanie {
         int r;//czas przygotowania
         int p;//czas na maszynie
         int q;//czas stygniecia
-        /*Zadanie(){r = p = q = kolejnosc = 0;}
-        void Zapisz(int kol, int er, int pe, int qu) {
-            kolejnosc = kol;
-            r = er;
-            p = pe;
-            q = qu;
-        }*/
 };
 Zadanie TabZadan[60]; //deklaracja tablicy zadan
 
@@ -30,9 +23,80 @@ ostream& operator<< (ostream& wyjscie, const Zadanie& zad){
     cout << zad.r << " - " << zad.p << " - " << zad.q << endl; 
     return wyjscie;
 }
+// 1-r, 2-p, 3-q
+void heapsort(Zadanie arr[], unsigned int N, int rpq){ //http://www.codecodex.com/wiki/Heapsort#C.2B.2B
+    unsigned int n = N, i = n/2, parent, child;  
+    Zadanie t;  
+    
+    for (;;) { /* Loops until arr is sorted */  
+        if (i > 0) { /* First stage - Sorting the heap */  
+            i--;           /* Save its index to i */  
+            t = arr[i];    /* Save parent value to t */  
+        } else {     /* Second stage - Extracting elements in-place */  
+            n--;           /* Make the new heap smaller */  
+            if (n == 0) return; /* When the heap is empty, we are done */  
+            t = arr[n];    /* Save last value (it will be overwritten) */  
+            arr[n] = arr[0]; /* Save largest value at the end of arr */  
+        }  
+    
+        parent = i; /* We will start pushing down t from parent */  
+        child = i*2 + 1; /* parent's left child */  
+    
+        /* Shift operation - pushing the value of t down the heap */  
+        while (child < n) { 
+            if (rpq==1){
+                if (child + 1 < n  &&  arr[child + 1].r > arr[child].r) {  
+                    child++; /* Choose the largest child */  
+                } 
+            }else if (rpq == 2){
+                if (child + 1 < n  &&  arr[child + 1].p > arr[child].p) {  
+                    child++; /* Choose the largest child */  
+                }
+            }else if (rpq == 3){
+                if (child + 1 < n  &&  arr[child + 1].q > arr[child].q) {  
+                    child++; /* Choose the largest child */  
+                }
+            }
+            //if (child + 1 < n  &&  arr[child + 1] > arr[child]) {  
+            //    child++; /* Choose the largest child */  
+            //}  
+            if (rpq==1){
+                if (arr[child].r > t.r) { /* If any child is bigger than the parent */  
+                    arr[parent] = arr[child]; /* Move the largest child up */  
+                    parent = child; /* Move parent pointer to this child */  
+                    //child = parent*2-1; /* Find the next child */  
+                    child = parent*2+1; /* the previous line is wrong*/  
+                } else {  
+                    break; /* t's place is found */  
+                }
+            }else if (rpq == 2){
+                if (arr[child].p > t.p) { /* If any child is bigger than the parent */  
+                    arr[parent] = arr[child]; /* Move the largest child up */  
+                    parent = child; /* Move parent pointer to this child */  
+                    //child = parent*2-1; /* Find the next child */  
+                    child = parent*2+1; /* the previous line is wrong*/  
+                } else {  
+                    break; /* t's place is found */  
+                } 
+            }else if (rpq == 3){
+                if (arr[child].q > t.q) { /* If any child is bigger than the parent */  
+                    arr[parent] = arr[child]; /* Move the largest child up */  
+                    parent = child; /* Move parent pointer to this child */  
+                    //child = parent*2-1; /* Find the next child */  
+                    child = parent*2+1; /* the previous line is wrong*/  
+                } else {  
+                    break; /* t's place is found */  
+                } 
+            }
+        }  
+        arr[parent] = t; /* We save t in the heap */  
+    }  
+}
+
+
 
 int main(){
-    int LiczbaZadan = 0, index = 0;
+    unsigned int LiczbaZadan = 0, index = 0;
     bool xFirst=1;
     string line, filename;
 
@@ -57,10 +121,10 @@ int main(){
         myfile.close();
     }else cout << "Unable to open file"; 
     // obsluga algorytmu, czyli petle zamieniajace kolejnosc dzialan
-    
+    heapsort(TabZadan,LiczbaZadan,1);
     // wyswietlenie kolejnosci:
     cout << endl<<"Wyliczona kolejnosc: ";
-    for (int i = 0; i<LiczbaZadan; i++){
+    for (unsigned int i = 0; i < LiczbaZadan; i++){
         cout << TabZadan[i].kolejnosc << " ";
     }
     cout << endl;
